2f2d3b8bf8468c45ebb2cdb95826658d64eeb581
Felipe Torres <92233132+Felipe-TM@users.noreply.github.com>
2023-11-08 16:46:30 -0300

    Ride Service Update
    Organized imports

--- ride-service/src/main/java/br/com/unifil/buscar/RideController.java
+++ ride-service/src/main/java/br/com/unifil/buscar/RideController.java
@@ -1,7 +1,5 @@
 package br.com.unifil.buscar;
 
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.maps.model.TrafficModel;
-import com.google.maps.model.TransitRoutingPreference;
 
 @RestController
 public class RideController {
